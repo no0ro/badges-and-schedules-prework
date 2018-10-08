a8a95c908c95c333ea390651a52e850ad4fc256e
Noelle Rolwes <nrolwes@gmail.com>
2018-10-08 10:45:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name) 
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(speakers) 
+  badge_message = []
+  speakers.each do |item|
+    badge_message << badge_maker(item)
+  end
+  badge_message
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  rooms = 1..7
+  speakers.each_with_index{|item,index| room_assignments << "Hello, #{item}! You'll be assigned to room #{index + 1}!"}
+  room_assignments
+end
+
+def printer(item)
+  item.each do |i|
+    puts batch_badge_creator(item)
+    puts assign_rooms(item)
+  end
+end 
